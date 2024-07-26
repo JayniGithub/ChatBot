0d9d71f6a98bb3b92524d9d99cec897d6027c981
Jayani Hettiarachchi <jayanihettiarachchi98@gmail.com>
2024-07-26 13:04:06 +0530

    Add new typing animation for response delay and applied solution for prevent unwanted button clicks

--- readme.md
+++ readme.md
@@ -1,20 +1,20 @@
 # PHP-Gemini Api Chatbot
-***
-This is simple chatbot which created using PHP and google gemini api. 
-***
+
+This is simple chatbot which created using PHP and google gemini api. There is typing animation for delay of response and initially submit button is disabled until you type something in input filed. Also once you submit anything,  button again will be disabled. That option will prevent unwanted button clicks.
+
 
 ## Tools & Technologies
-***
+
 * Front-end -> HTML, CSS, JavaScript, AJAX
 * Backend - PHP
 * Google Gemini API
 
 ## Google Gemini API
-***
+
 For this chatbot, you need to create an api key to access gemini api. You can create an api key by visiting https://aistudio.google.com/app/apikey. Of cause you need to have Google account for that. You can grab an API key for free of charge under some limitations. Also you can use billed option as well.
 
 ## Project Setup
-***
+
 * Clone this project into your C:/xampp/htdoct folder
 * Then open it in code editor and open message.php file
 * Replace your API key in below showed place in message.php file
--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+    $('#send-btn').prop('disabled', true);
+    $('#data').keyup(function() {
+        if($(this).val().length != 0) {
+            $('#send-btn').prop('disabled', false);
+        }
+    });
     $("#send-btn").on("click", function(){
         $value = $("#data").val();
         $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>'+ $value +'</p></div></div>';
@@ -10,9 +16,17 @@ $(document).ready(function(){
             url: 'message.php',
             type: 'POST',
             data: 'text='+$value,
+            beforeSend: function() {
+                $loading = '<div class="bot-inbox inbox" id="loading"><div class="icon"><img src="./img/chatbot old.jpg" style="width:45px;"/></div><div class="loading-anim-div"><div class="dot"></div><div class="dot"></div><div class="dot"></div></div></div>';
+                $(".form").append($loading);
+            },            
+            complete:  function(){
+                $('#loading').remove();
+            },
             success: function(result){
                 $response = '<div class="bot-inbox inbox"><div class="icon"><img src="./img/chatbot old.jpg" style="width:45px;"/></div><div class="msg-header"><p>'+ result +'</p></div></div>';
                 $(".form").append($response);
+                $('#send-btn').prop('disabled', true);
             }
         });
     });
